66a3c7eda30e45153d357e039a0ee9578b7dd6e2
Meshivu02 <161490810+Meshivu02@users.noreply.github.com>
2024-03-06 16:33:02 +0530

    Update DynamicKeypad.ino

--- DynamicKeypad.ino
+++ DynamicKeypad.ino
@@ -10,19 +10,22 @@ char keys[ROW_NUM][COLUMN_NUM] = {
   {'*', '0', '#', 'D'}
 };
 
-byte pin_rows[ROW_NUM] = {9, 8, 7, 6}; // connect to the row pinouts of the keypad
-byte pin_column[COLUMN_NUM] = {5, 4, 3, 2}; // connect to the column pinouts of the keypad
+byte pin_rows[ROW_NUM] = {9, 8, 7, 6};    // Connect to the row pinouts of the keypad
+byte pin_column[COLUMN_NUM] = {5, 4, 3, 2}; // Connect to the column pinouts of the keypad
 
 Keypad keypad = Keypad(makeKeymap(keys), pin_rows, pin_column, ROW_NUM, COLUMN_NUM);
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(9600); // Initialize the serial communication
+
+  // Setup the keypad with specified pin configurations
+  keypad.setDebounceTime(10);
 }
 
 void loop() {
-  char key = keypad.getKey();
+  char key = keypad.getKey(); // Read the key pressed on the keypad
 
   if (key) {
-    Serial.println(key);
+    Serial.println(key); // Print the pressed key to the Serial Monitor
   }
 }
